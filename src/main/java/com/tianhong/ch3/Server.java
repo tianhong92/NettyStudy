c42159fa0df0c4dcbec1c853c849abb4588ddffe
tianhong <tianhong229@gmail.com>
2018-10-16 15:10:42 +0800

    WIP: netty demo

---
+++ src/main/java/com/tianhong/ch3/Server.java
@@ -0,0 +1,40 @@
+package com.tianhong.ch3;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.AttributeKey;
+
+
+public final class Server {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    //给每个客户端连接设置tcp基本属性
+                    .childOption(ChannelOption.TCP_NODELAY, true)
+                    //给每个客户端连接绑定基本属性
+                    .childAttr(AttributeKey.newInstance("childAttr"), "childAttrValue")
+                    .handler(new ServerHandler())
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            //ch.pipeline().addLast(new ServerHandler());
+                        }
+                    });
+            ChannelFuture f = b.bind(8888).sync();
+            f.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
